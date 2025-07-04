91f391ef2ab17dfba06993cd1302810311d5fa58
frubes05 <filip.rubes2@gmail.com>
2025-07-04 22:45:14 +0200

    Remove unecessary comment

--- server/middlewares/error-handling.ts
+++ server/middlewares/error-handling.ts
@@ -6,8 +6,6 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ): void => {
-  console.error("💥 Uncaught error:", err);
-
   res.status(500).json({
     error: "Something went wrong on the server.",
     details: process.env.NODE_ENV === "development" ? err.message : undefined,
