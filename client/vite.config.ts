8a833de9c5f8d4109c866f122427fa13fdc018dd
frubes05 <filip.rubes2@gmail.com>
2025-07-05 12:27:53 +0200

    Use correct toggle for modes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/movie-library-app/",
-});
+  base: mode === "production" ? "/movie-library-app/" : "/",
+}));
