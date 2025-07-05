d67cc71e027cf2d97baa38f43311781e87568eda
frubes05 <filip.rubes2@gmail.com>
2025-07-05 17:37:44 +0200

    Add basic SEO

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,18 +6,15 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { GlobalContextProvider } from "./context/global";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      <Provider store={store}>
-        <BrowserRouter>
-          <GlobalContextProvider>
-            <App />
-          </GlobalContextProvider>
-        </BrowserRouter>
-      </Provider>
-    </HelmetProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalContextProvider>
+          <App />
+        </GlobalContextProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
