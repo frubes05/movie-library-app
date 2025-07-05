a8dcbda0c486bd47a24de6fef11aeaa4e3168101
frubes05 <filip.rubes2@gmail.com>
2025-07-05 17:36:55 +0200

    Add basic SEO

--- client/src/pages/main-page/main-page.tsx
+++ client/src/pages/main-page/main-page.tsx
@@ -3,8 +3,6 @@ import { useGlobalContext } from "../../hooks/global";
 import { MoviesContextProvider } from "../../context/movies/provider";
 import MoviesWrapper from "../../features/movies/movies-wrapper/movies-wrapper";
 import SearchWrapper from "../../features/search/search-wrapper/search-wrapper";
-import HeadSEO from "../../components/head-seo/head-seo";
-import { useLocation } from "react-router-dom";
 
 const MainPage = () => {
   const {
@@ -15,36 +13,28 @@ const MainPage = () => {
     onSubmitSearch,
     isMobile,
   } = useGlobalContext();
-  const location = useLocation();
 
   return (
-    <>
-      <HeadSEO
-        key={location.pathname + location.search + page}
-        searchQuery={searchQuery}
-        page={page}
-      />
-      <main>
-        <MoviesContextProvider
-          movieData={{ page, searchQuery, isSearching: Boolean(searchQuery) }}
-        >
-          <MainLayout
-            title="Movies Library"
-            searchSection={
-              <SearchWrapper
-                query={searchQuery}
-                onInputChange={onInputChange}
-                onSubmitSearch={onSubmitSearch}
-                isMobile={isMobile}
-              />
-            }
-            content={
-              <MoviesWrapper onPageChange={onPageChange} isMobile={isMobile} />
-            }
-          />
-        </MoviesContextProvider>
-      </main>
-    </>
+    <main>
+      <MoviesContextProvider
+        movieData={{ page, searchQuery, isSearching: Boolean(searchQuery) }}
+      >
+        <MainLayout
+          title="Movies Library"
+          searchSection={
+            <SearchWrapper
+              query={searchQuery}
+              onInputChange={onInputChange}
+              onSubmitSearch={onSubmitSearch}
+              isMobile={isMobile}
+            />
+          }
+          content={
+            <MoviesWrapper onPageChange={onPageChange} isMobile={isMobile} />
+          }
+        />
+      </MoviesContextProvider>
+    </main>
   );
 };
 
